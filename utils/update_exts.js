000a2cb589ac355540806ae235f39ebdf73c99bf
Tomas Kubla <tomas@kubla.sk>
2021-03-14 23:36:12 +0000

    Add broadcast call

--- utils/update_exts.js
+++ utils/update_exts.js
@@ -13,49 +13,62 @@ module.exports = function (callback){
     user: 'root',
     host: 'idefix.gjh.sk',
     port: '22',
-    path: '/etc/asterisk/test'
+    path: '/etc/asterisk/extensions.conf'
   };
-  
-  fs.copySync('headers/'+filename, filename);
+  try { 
+    fs.copySync('headers/'+filename, filename);
+    var query = "SELECT E.Ext as Ext, E.Name as Description, group_concat(concat('SIP/',P.Login) separator '&') as Dial, E.Scenario as Scenario FROM Exts as E RIGHT JOIN Ext2Phones as E2P ON E.ID=E2P.ID_Ext JOIN Phones as P ON E2P.ID_Phone=P.ID WHERE E.Scenario is NULL GROUP BY E.ID ORDER BY E.Ext";
 
-  var query = "SELECT E.Ext as Ext, E.Name as Description, group_concat(concat('SIP/',P.Login) separator '&') as Dial, E.Scenario as Scenario FROM Exts as E RIGHT JOIN Ext2Phones as E2P ON E.ID=E2P.ID_Ext JOIN Phones as P ON E2P.ID_Phone=P.ID WHERE E.Scenario is NULL GROUP BY E.ID ORDER BY E.Ext";
+    var query = connection.query(query, function(err, result) {
+      if (!err){ 
+        var wstream = fs.createWriteStream(filename,{'flags': 'a'});
+        result.forEach(function(row){
 
-  var query = connection.query(query, function(err, result) {
-    if (!err){ 
-      var wstream = fs.createWriteStream(filename,{'flags': 'a'});
-      result.forEach(function(row){
+          ext = row.Ext
+          description = row.Description;
+          dial = row.Dial;
+          wstream.write('; '+description+' \n');
+          wstream.write('exten => '+ext+',1,Dial('+dial+')\n');
+          wstream.write('   same => n,Hangup()\n\n');
 
-        ext = row.Ext
-        description = row.Description;
-        dial = row.Dial;
-        wstream.write('; '+description+' \n');
-        wstream.write('exten => '+ext+',1,Dial('+dial+')\n');
-        wstream.write('   same => n,Handup()\n\n');
-
-      });
-      wstream.end();
-      scp.send(options, function (err) {
-        if (!err){
-          fs.unlink(filename);
-          ami.action( 'reload', null, function(data){
-            if(data.Response == 'Error'){
-              callback('Error');
-              console.log('reload', data.Message);
-            }
-            else
-               callback();
-          });
-        }
-        else{
-          callback('Error');
-          console.log(err);
-          fs.unlink(filename);
-        }
-      });
-    }else{
-      console.log('Error while performing Query.',err);
-      callback('Error');
-    }
-    connection.end();
-  });
+        });
+        var query = "SELECT GROUP_CONCAT(concat('SIP/',Login) ORDER BY Login SEPARATOR '&') as gs FROM `Phones`;";
+        var query = connection.query(query, function(err, result) {
+          if (!err){ 
+            wstream.write('; broadcast call\n');
+            wstream.write('exten => 900,1,SIPAddHeader(Call-Info:\\;answer-after=0)\n');
+            wstream.write('   same => n,Page('+result[0].gs+')\n');
+            wstream.write('   same => n,Hangup()\n');
+            wstream.end();
+            scp.send(options, function (err) {
+              if (!err){
+                fs.unlink(filename);
+                ami.action( 'reload', null, function(data){
+                  if(data.Response == 'Error'){
+                    callback('Error');
+                    console.log('reload', data.Message);
+                  }
+                  else
+                     callback();
+                });
+              }
+              else{
+                callback('Error');
+                console.log(err);
+              }
+            });
+          }else{
+            console.log('Error while performing Query.',err);
+            callback('Error');
+          }
+        });
+      }else{
+        console.log('Error while performing Query.',err);
+        callback('Error');
+      }
+      connection.end();
+    });
+  } catch(err) {
+    console.error(err)
+  }
 }
