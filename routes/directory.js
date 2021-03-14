abdacd9085977627531ca42d393bfe7b39c42d60
Tomas Kubla <tomas@kubla.sk>
2021-03-14 23:44:11 +0000

    Fix asc mapping

--- routes/directory.js
+++ routes/directory.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
   ip = req.headers['x-forwarded-for'];
   pc(ip,function(perm){
 
-    connection.query('SELECT * FROM(SELECT concat(t.firstname," ",t.lastname) as Meno, c.name as Kabinet, ext as Klapka FROM aScCab2Ext as ac2e JOIN Exts as e ON e.ID = ac2e.ID_Ext JOIN identitySync.classroomsX as c ON c.id = ac2e.ID_aScCab LEFT JOIN identitySync.teachersX AS t ON c.id = t.classroomid UNION ALL SELECT concat(firstname," ",lastname) as name, null, ext FROM aScTea2Ext as at2e JOIN Exts as e ON e.ID = at2e.ID_Ext JOIN identitySync.teachersX as t ON t.id = at2e.ID_aScTea) as UN ORDER BY Klapka', function(err, rows, fields) {
+    connection.query('SELECT Meno, Kabinet, Klapka FROM(SELECT concat(t.firstname," ",t.lastname) as Meno, c.name as Kabinet, ext as Klapka, lastname as Lastname, firstname as firstname FROM aScCab2Ext as ac2e JOIN Exts as e ON e.ID = ac2e.ID_Ext JOIN identitySync.classrooms as c ON c.id = ac2e.ID_aScCab LEFT JOIN identitySync.teachers AS t ON c.id = t.classroomid UNION ALL SELECT concat(firstname," ",lastname) as name, "Osobná klapka", ext, lastname, firstname FROM aScTea2Ext as at2e JOIN Exts as e ON e.ID = at2e.ID_Ext JOIN identitySync.teachers as t ON t.id = at2e.ID_aScTea) as UN ORDER BY UN.Lastname, UN.firstname, UN.Kabinet, UN.Klapka', function(err, rows, fields) {
       if (!err){      
         res.render('directory', { title: 'Directory', rows: rows, fields: fields, perm: perm});  
       }else{
@@ -22,4 +22,4 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
-//SELECT c.name as Meno, ext as Klapka FROM aScCab2Ext as ac2e JOIN Exts as e ON e.ID = ac2e.ID_Ext JOIN identitySync.classroomsX as c ON c.id = ac2e.ID_aScCab UNION ALL SELECT concat(firstname," ",lastname) as name,ext FROM aScTea2Ext as at2e JOIN Exts as e ON e.ID = at2e.ID_Ext JOIN identitySync.teachersX as t ON t.id = at2e.ID_aScTea
+//SELECT c.name as Meno, ext as Klapka FROM aScCab2Ext as ac2e JOIN Exts as e ON e.ID = ac2e.ID_Ext JOIN identitySync.classrooms as c ON c.id = ac2e.ID_aScCab UNION ALL SELECT concat(firstname," ",lastname) as name,ext FROM aScTea2Ext as at2e JOIN Exts as e ON e.ID = at2e.ID_Ext JOIN identitySync.teachers as t ON t.id = at2e.ID_aScTea
